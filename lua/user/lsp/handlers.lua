e4dacb5d4b7302feb5d889d50083c641198f8eff
TurtleS <quy.doanduy@gmail.com>
2021-12-24 23:44:27 +0700

    add telescope

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -96,10 +96,10 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-    print("failed to load cmp_nvim_lsp")
   return
 end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
+
