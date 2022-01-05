14e9a5ffd2be54c8e8cb30ecb576a663c2d1b805
quy.doanduy <quy.doanduy@gmail.com>
2022-01-05 08:56:46 +0700

    fix flutter lsp in process

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -3,7 +3,7 @@ require('bufferline').setup {
     indicator_icon = '▎',
     buffer_close_icon = '',
     modified_icon = '●',
-    close_icon = '',
+    close_icon = '',
     left_trunc_marker = '',
     name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
     -- remove extension from markdown files for example
